b910d935c8f5c84f8ec0967cda7601504432d1ae
Maxime François <mfrancois@tequilarapido.com>
2013-12-10 00:13:13 +0100

    Add rules for project view and test

---
+++ app/controllers/ProjectController.php
@@ -0,0 +1,29 @@
+<?php
+
+class ProjectController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($project,$file)
+	{
+		//
+        var_dump($project);
+        var_dump($file);
+        exit;
+	}
+
+}
--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,6 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+
+Route::get('/', 'ProjectController@index');
+Route::get('{firstPart}/{rest}', 'ProjectController@show')->where('firstPart', '[^/]*')->where('rest', '.*');
